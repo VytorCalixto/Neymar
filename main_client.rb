497f83dbd8cf415ae9c8f84a83f345f5940025be
Israel B. Sant'Anna <ibsa14@inf.ufpr.br>
2016-06-01 20:24:44 -0300

    Variable number of messages working. Added 25 more machines
    Signed-off-by: Israel B. Sant'Anna <ibsa14@inf.ufpr.br>

--- main_client.rb
+++ main_client.rb
@@ -8,22 +8,25 @@ if ARGV.length < 1
 end
 
 server = ARGV[0]
-n = ARGV[1].to_i
 client = Client.new(server,Configuration::PORT)
 
 tweets_text = File.read(File.join(File.dirname(__FILE__), "tweets.json"))
 tweets = JSON.parse(tweets_text)["tweets"]
 
-if n < tweets.size
-  client.messages = tweets[0..n-1]
+if ARGV.length < 2
+  client.messages = tweets
 else
-  repeat = n/tweets.size
-  repeat.times { client.messages.concat tweets }
-  mod = n%tweets.size
-  if mod != 0
-    client.messages.concat tweets[0..mod]
+  n = ARGV[1].to_i
+  if n < tweets.size
+    client.messages = tweets[0..n-1]
+  else
+    repeat = n/tweets.size
+    repeat.times { client.add_messages tweets }
+    mod = n%tweets.size
+    if mod != 0
+      client.add_messages tweets[0..mod-1]
+    end
   end
 end
-    
 p client.messages
 client.send_all
