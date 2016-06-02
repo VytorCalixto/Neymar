b3c137223cd1271c8dc6308563c6822cafe7fe82
Vytor Calixto <vytorcalixto@gmail.com>
2016-06-02 10:32:43 -0300

    Using optparse on console

--- console.rb
+++ console.rb
@@ -1,22 +1,52 @@
 require 'socket'
 require 'json'
 require 'logger'
+require 'optparse'
 require_relative 'client'
 require_relative 'configuration'
 
-if ARGV.length != 1
-  puts "Uso correto: ruby console.rb <servidor>"
-  abort
+puts "-=NEYMAR CONSOLE=-"
+
+options = {}
+optparse = OptionParser.new do |opts|
+  opts.banner = "Uso correto: ruby console.rb [options]"
+
+  opts.on("-s", "--server [NAME]", String, "Nome do servidor (obrigatório)") do |s|
+    options[:server] = s
+  end
+
+  opts.on("-p", "--ping", "Verifica as máquinas disponíveis para usar como cliente") do |i|
+    options[:ping] = i
+  end
+
+  opts.on("-h", "--help", "Imprime a ajuda") do |h|
+    puts opts
+    exit
+  end
+end
+
+begin
+  optparse.parse!
+  mandatory = [:server]
+  missing = mandatory.select{ |param| options[param].nil? }
+  unless missing.empty?
+    puts "Opções obrigratórias: #{missing.join(', ')}"
+    puts optparse
+    exit
+  end
+rescue OptionParser::InvalidOption, OptionParser::MissingArgument
+  puts $!.to_s
+  puts optparse
+  exit
 end
 
 file = File.open('neymar.log', File::WRONLY | File::APPEND)
 log = Logger.new(file)
 
-server = ARGV[0]
+server = options[:server]
 
 client = Client.new(server,Configuration::PORT)
 
-puts "-=NEYMAR CONSOLE=-"
 
 commands = [{:cmd => '\q', :desc => "Parar o servidor"},
             {:cmd => '\s', :desc => "Imprimir status do servidor"},
@@ -26,20 +56,22 @@ commands = [{:cmd => '\q', :desc => "Parar o servidor"},
 machines_text = File.read(File.join(File.dirname(__FILE__), "machines"))
 machines = machines_text.split("\n")
 
-log.info('Console') {"Verificando disponibilidade de #{machines.size} possíveis clientes"}
-print "Verificando máquinas"
-machines.delete_if do |m|
-    print "."
-    STDOUT.flush
-    log.debug('Console') {"Ping em #{m}"}
-    ping = `ping -q -c 2 #{m}`
-    if $?.exitstatus != 0
-        log.debug('Console') {"#{m} não responde"}
-        true
-    end
+unless options[:ping].nil?
+  log.info('Console') {"Verificando disponibilidade de #{machines.size} possíveis clientes"}
+  print "Verificando máquinas"
+  machines.delete_if do |m|
+      print "."
+      STDOUT.flush
+      log.debug('Console') {"Ping em #{m}"}
+      ping = `ping -q -c 1 #{m} > /dev/null`
+      if $?.exitstatus != 0
+          log.debug('Console') {"#{m} não responde"}
+          true
+      end
+  end
+  print "\r"
+  log.info('Console') {"#{machines.size} máquinas disponíveis"}
 end
-print "\r"
-log.info('Console') {"#{machines.size} máquinas disponíveis"}
 
 puts "Existem #{machines.size} máquinas disponíveis."
 
